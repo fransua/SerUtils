a1e637d7f29bf4711acc23ec4a147a4730365691
fransua <serra.francois@gmail.com>
2022-03-25 02:35:39 +0100

    added single-cell subsampler from FASTQ

--- serutils/sequence/fastq_utils.py
+++ serutils/sequence/fastq_utils.py
@@ -5,6 +5,8 @@
 from subprocess                           import Popen, PIPE
 from random                               import random
 from os                                   import SEEK_END
+from re                                   import compile as re_compile, MULTILINE
+from nbformat import read
 
 from numpy                                import mean, std
 
@@ -39,7 +41,7 @@ def count_reads_approx(fnam, samples=1000, verbose=True):
     :returns: number of reads estimated
     """
     fhandler = open(fnam)
-    fhandler.seek(0, SEEK_END)
+    fhandler.seek(0, SEEK_END)  # go at the end of the file
     flen = fhandler.tell()
     values = []
     def _read_size(rnd):
@@ -100,6 +102,113 @@ def estimate_cardinality(values, k):
         bucket_hash = h >> k
         max_zeroes[bucket] = max(max_zeroes[bucket], _trailing_zeroes(bucket_hash))
     return 2 ** (float(sum(max_zeroes)) // num_buckets) * num_buckets * 0.79402
+
+
+def sc_coordinates(fname, reading_chunk=1000, ncells=96, 
+                   regex="^(@).*:([0-9]+):[0-9]+:[0-9]+ .*\n[ACGTN]+\n\+\n.*$"):
+    """
+    index single-cells in FASTQ file
+    
+    :param fname: path to FASTQ
+    :param 1000 reading_chunk: 2 entries should be contained is this length
+    :param 96 ncells: number of cells sequenced and represented in the FASTQ
+    :param 1000 sub_sample_size: wanted number of entries per cell
+    :param regex: to identify cell identifier in read-ID
+
+    :returns: a dictionnary with, as values, the start and end byte position 
+       of each cell
+    """
+
+    regex = re_compile(regex, flags=MULTILINE)  # MULTILINE allows the use of ^ and $
+
+    def get_cell_id(substring):
+        reg_iter = regex.finditer(substring)
+        match = next(reg_iter)
+        pos1 = match.start()
+        read1 = match[2]
+        match = next(reg_iter)
+        pos2 = match.start()
+        read2 = match[2]
+        return read1, pos1, read2, pos2
+
+    fhandler = open(fname)
+    fhandler.seek(0, SEEK_END)
+    flen = fhandler.tell()
+    positions = {}
+    fhandler.seek(0)
+    prev_id, *_ = get_cell_id(fhandler.read(reading_chunk))
+    positions[prev_id] = {'start': [0]}
+    pos = flen // ncells
+    top = 0
+    bot = flen
+    while True:
+        fhandler.seek(pos)
+        try:
+            id1, pos1, id2, pos2 = get_cell_id(fhandler.read(reading_chunk))
+        except StopIteration:
+            positions[prev_id].setdefault('end', []).append(bot)
+            positions[prev_id].setdefault('end', []).append(bot)
+            break
+        if prev_id != id1:  # we should look up
+            bot = pos2 + pos
+            pos -= (pos + pos1 - top) // 2
+        elif prev_id == id2:  # we should look down
+            top = pos + pos1
+            pos += (bot - pos - pos1) // 8  # should be 2, but we want to make sure to not overjump -> readids are repeated
+        else:
+            pos += pos2
+            positions[prev_id].setdefault('end', []).append(pos)
+            prev_id = id2
+            if prev_id in positions:
+                positions[prev_id].setdefault('start', []).append(pos)
+            else:
+                positions[prev_id] = {'start': [pos]}
+            top = pos
+            bot = flen
+    fhandler.close()
+    if len(positions) != ncells:
+        print(f'WARNING: expected number of cells ({ncells}) not matching number found: {len(positions)}')
+    return positions
+
+
+def subsample_SC(fname, outfname, reading_chunk=1000, ncells=96, sub_sample_size=1000,
+                 regex="^(@).*:([0-9]+):[0-9]+:[0-9]+ .*\n[ACGTN]+\n\+\n.*$"):
+    """
+    subsample single-cell FASTQ files
+    
+    :param fname: path to FASTQ
+    :param outfname: path to subsampled output FASTQ
+    :param 1000 reading_chunk: 2 entries should be contained is this length
+    :param 96 ncells: number of cells sequenced and represented in the FASTQ
+    :param 1000 sub_sample_size: wanted number of entries per cell
+    :param regex: to identify cell identifier in read-ID
+
+    *WARNING: should be tested with very large chunks to check that input and 
+       output contain the same*
+    """
+    coords = sc_coordinates(fname, reading_chunk=reading_chunk, ncells=ncells, 
+                            regex=regex)
+    fhandler = open(fname)
+    outfh = open(outfname, 'w')
+    # for each cell
+    for c in coords:
+        count = 0
+        # for each file chunk containt current cell
+        for beg, end in zip(coords[c]['start'], coords[c]['end']):
+            fhandler.seek(beg)
+            pos = beg
+            for line in fhandler:
+                line += next(fhandler)
+                line += next(fhandler)
+                line += next(fhandler)
+                pos += len(line)
+                outfh.write(line)
+                count += 1
+                if pos == end or count == sub_sample_size:
+                    break
+            if count == sub_sample_size:
+                break
+    outfh.close()
 
 
 def main():
